f3a09ed2ed671d1d7b3229c81e90af62c15ee06e
Kode <Olorik362@gmail.com>
2025-04-21 01:25:38 +0100

    updated installe apps

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['void-backend-hb66.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
